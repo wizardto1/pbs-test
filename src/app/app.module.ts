39edc6bfc2a905b833e63f6dedf1330645ad710c
wizardto1 <ph429@drexel.edu>
2020-08-19 16:23:28 -0400

    2 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,15 +11,17 @@ import { HeaderComponent } from './header/header.component';
 import { InfoComponent } from './info/info.component';
 import { Page1Component } from './page1/page1.component';
 import { TimerComponent } from './timer/timer.component';
+import { Page2Component } from './page2/page2.component';
+
 
 
 const appRoutes: Routes = [
-  { path: '', redirectTo : '/home', pathMatch:'full'},{path:'home', component: HomeComponent, pathMatch:'full' },{path:'start',component:StartComponent},{path:'info',component:InfoComponent},{path:'page1',component:Page1Component}
+  { path: '', redirectTo : '/home', pathMatch:'full'},{path:'home', component: HomeComponent, pathMatch:'full' },{path:'start',component:StartComponent},{path:'info',component:InfoComponent},{path:'page1',component:Page1Component},{path:'page2',component:Page2Component}
 ];
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule , RouterModule.forRoot(appRoutes)],
-  declarations: [ AppComponent, HelloComponent, StartComponent, HomeComponent, HeaderComponent, InfoComponent, Page1Component, TimerComponent,],
+  declarations: [ AppComponent, HelloComponent, StartComponent, HomeComponent, HeaderComponent, InfoComponent, Page1Component, TimerComponent, Page2Component,],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/page2/page2.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-page2',
+  templateUrl: './page2.component.html',
+  styleUrls: ['./page2.component.css']
+})
+export class Page2Component implements OnInit {
+ flag;
+  time;
+  buttonDisabled:boolean=false;
+  constructor() { }
+
+  ngOnInit() {
+  }
+radioEventHandler(event:any){
+    
+    this.buttonDisabled=true;
+  }
+}
