fd06f568d26eac1f44783b6603a75024e9b3c10c
wizardto1 <ph429@drexel.edu>
2020-08-19 21:17:42 -0400

    3 files modified

--- src/app/page20/page20.component.ts
+++ src/app/page20/page20.component.ts
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./page20.component.css']
 })
 export class Page20Component implements OnInit {
-
+flag;
+  time;
+  buttonDisabled:boolean=false;
   constructor() { }
 
   ngOnInit() {
   }
-
+radioEventHandler(event:any){
+    
+    this.buttonDisabled=true;
+  }
 }
