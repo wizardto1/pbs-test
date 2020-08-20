b25c9f703175abef6400db918c9754703eff693e
wizardto1 <ph429@drexel.edu>
2020-08-19 22:13:44 -0400

    1 file modified, 2 added

---
+++ src/app/results/results.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-results',
+  templateUrl: './results.component.html',
+  styleUrls: ['./results.component.css']
+})
+export class ResultsComponent implements OnInit {
+  qs=0;
+  so=0;
+  apa=0;
+  pr=0;
+  sg=0;
+  total=0;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
